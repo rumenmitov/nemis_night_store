238cb969c203737352ea12600b3cadbf199917ee
rumen <rumen.valmitov@gmail.com>
2023-03-14 17:07:16 +0100

    Frontend finishing touches ✨

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-
+  goToInstagram() {
+    location.href = 'https://www.instagram.com/nemisnight/';
+  }
 }
