405ba6e2044a13c298d7e5ac3ccb63669ded99f4
rumen <rumen.valmitov@gmail.com>
2023-03-14 14:10:30 +0100

    Adding footer 🦶

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
